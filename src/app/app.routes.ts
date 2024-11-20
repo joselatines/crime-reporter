6b9bb10d40a1273e80faba92e81513ae6a707b29
caladavid <davidcalae@gmail.com>
2024-11-20 16:09:46 -0400

    created component folders pages/news, pages/notes, pages/map

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NewsComponent } from './pages/news/news.component';
+import { NotesComponent } from './pages/notes/notes.component';
+import { MapComponent } from './pages/map/map.component';
 
 export const routes: Routes = [
     { path: "login", component: LoginComponent },
     { path: "dashboard", component: DashboardComponent },
+    { path: "news", component: NewsComponent },
+    { path: "notes", component: NotesComponent },
+    { path: "map", component: MapComponent },
 ];
