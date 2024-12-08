03305c721a848e2e1c163c77b8af00975c95056e
Eliezer <eliezerandrespc@gmail.com>
2024-12-07 22:53:14 -0400

    herramienta OCR

--- src/app/pages/ocr/ocr.component.ts
+++ src/app/pages/ocr/ocr.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import * as Tesseract from 'tesseract.js';
 
 @Component({
   selector: 'app-ocr',
-  standalone: true,
-  imports: [],
   templateUrl: './ocr.component.html',
-  styleUrl: './ocr.component.css'
+  styleUrls: ['./ocr.component.css']
 })
 export class OcrComponent {
+  imageFile: File | null = null;
+  ocrResult: string = ''; // Variable para almacenar el texto extraído
+  loading: boolean = false; // Indicador de carga
 
+  uploadImage(event: any) {
+    this.imageFile = event.target.files[0];
+    this.extractText();
+  }
+
+  extractText() {
+    if (this.imageFile) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const imageData = e.target.result;
+        this.loading = true; // Iniciar el indicador de carga
+
+        Tesseract.recognize(
+          imageData,
+          'eng', // Cambia el idioma según sea necesario
+          {
+            logger: info => console.log(info) // Para ver el progreso (opcional)
+          }
+        ).then(({ data: { text } }) => {
+          this.ocrResult = text; // Asigna el texto extraído a la variable
+          this.loading = false; // Finalizar el indicador de carga
+        }).catch(err => {
+          console.error("Error durante el reconocimiento:", err);
+          this.loading = false; // Finalizar el indicador de carga en caso de error
+        });
+      };
+      reader.readAsDataURL(this.imageFile);
+    }
+  }
 }
