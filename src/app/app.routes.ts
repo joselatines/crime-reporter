798d7f789d49f73bd9880cee73a422f0ae62d0d5
ChristopherZ83 <christopherzapata600@gmail.com>
2025-01-06 13:56:35 -0400

    register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,11 @@ import { NewsComponent } from './pages/news/news.component';
 import { NotesComponent } from './pages/notes/notes.component';
 import { MapComponent } from './pages/map/map.component';
 import { OcrComponent } from './pages/ocr/ocr.component';
+import { RegisterComponent } from './auth/login/register/register.component';
 
 export const routes: Routes = [
     { path: "login", component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: "dashboard", component: DashboardComponent },
     { path: "news", component: NewsComponent },
     { path: "notes", component: NotesComponent },
