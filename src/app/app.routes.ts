eeb1dd254caf2e20ff1abe73c350f0485713037a
caladavid <davidcalae@gmail.com>
2025-02-04 00:13:46 -0400

    UI fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NewsComponent } from './pages/news/news.component';
-import { NotesComponent } from './pages/notes/notes.component';
 import { MapComponent } from './pages/map/map.component';
 import { OcrComponent } from './pages/ocr/ocr.component';
 import { RegisterComponent } from './auth/register/register.component';
